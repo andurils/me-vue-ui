0a3c000d390b1f0e705e6253f6aea88eef53abf5
Anduril <76253631@qq.com>
2021-02-25 11:36:53 +0800

    webpack 打包运行
    webpack 打包运行

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,48 +1,46 @@
 const path = require("path");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV,
-  entry: "./examples/main.js",
-  output: {
-    path: path.resolve(process.cwd(), "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-        options: {
-          compilerOptions: {
-            preserveWhitespace: false,
-          },
-        },
-      },
-      // 它会应用到普通的 `.js` 文件
-      // 以及 `.vue` 文件中的 `<script>` 块
-      // {
-      //   test: /\.js$/,
-      //   loader: "babel-loader",
-      // },
-      // 它会应用到普通的 `.css` 文件
-      // 以及 `.vue` 文件中的 `<style>` 块
-      {
-        test: /\.css$/,
-        use: ["vue-style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192,
+    mode: process.env.NODE_ENV,
+    entry: "./examples/main.js",
+    output: {
+        path: path.resolve(process.cwd(), "dist"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    compilerOptions: {
+                        preserveWhitespace: false
+                    }
+                }
             },
-          },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                loader: 'url-loader',
+            },
         ],
-      },
-    ],
-  },
-  plugins: [new VueLoaderPlugin()],
+    },
+    plugins: [new VueLoaderPlugin(), new HtmlWebpackPlugin({
+        title: 'Development',
+        favicon: './public/favicon.ico',
+        template: 'public/index.html', //path.join(process.cwd(), 'public/index.html'), 
+        filename: 'index.html',
+        inject: true,
+    }),],
 };
